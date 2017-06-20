5f03072d580c0dee354d101f4e77b5fafe209fe5
GRANIER Julien <julien.granier@grsvaltech.fr>
2017-06-20 11:12:13 +0200

    Regenerated latest sources

--- dist/keen-ui.js
+++ dist/keen-ui.js
@@ -1015,16 +1015,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	    value: true
 	});
 	
-	var _classlist = __webpack_require__(23);
+	var _classlist2 = __webpack_require__(23);
 	
-	var _classlist2 = _interopRequireDefault(_classlist);
+	var _classlist3 = _interopRequireDefault(_classlist2);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	var startRipple = function startRipple(eventType, event) {
 	    var holder = event.currentTarget || event.target;
 	
-	    if (holder && !_classlist3.default.has(holder, 'ui-ripple-ink')) {
+	    if (holder && !_classlist.default.has(holder, 'ui-ripple-ink')) {
 	        holder = holder.querySelector('.ui-ripple-ink');
 	    }
 	
@@ -1071,7 +1071,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    holder.appendChild(ripple);
 	
 	    setTimeout(function () {
-	        _classlist2.default.add(ripple, 'held');
+	        _classlist3.default.add(ripple, 'held');
 	    }, 0);
 	
 	    var releaseEvent = eventType === 'mousedown' ? 'mouseup' : 'touchend';
@@ -1079,13 +1079,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var release = function release() {
 	        document.removeEventListener(releaseEvent, release);
 	
-	        _classlist2.default.add(ripple, 'done');
+	        _classlist3.default.add(ripple, 'done');
 	
 	        setTimeout(function () {
 	            holder.removeChild(ripple);
 	
 	            if (!holder.children.length) {
-	                _classlist2.default.remove(holder, 'active');
+	                _classlist3.default.remove(holder, 'active');
 	                holder.removeAttribute('data-ui-event');
 	            }
 	        }, 450);
